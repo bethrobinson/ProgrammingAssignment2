16894d15c579fe2d5db986e1e6a0c6c92f7dcea4
Beth Robinson <bethjrobinson@gmail.com>
2014-07-27 19:30:21 -0400

    Added a solution to this week's assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions can be used when the inverse of a matrix
+## may need to be calculated repeatedly with data that may or may not change. 
+## It will cache the solution to be called later when needed,
+## saving on overall calculation time.
 
-## Write a short comment describing this function
-
-## Test file for trying out commits.
+## Create a set of four small functions stored in a list.
+## Each function can be called separately.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   m <- NULL
+   set <- function(y) {
+      x <<- y
+      m <<- NULL
+   }
+   get <- function() x
+   setinvert <- function(solve) m <<- solve
+   getinvert <- function() m
+   list(set = set, get = get,
+        setinvert <- setinvert,
+        getinvert <- getinvert)
 }
 
 
-## Write a short comment describing this function
+## This function solves for the inverse of a set of data in a matrix
+## If the inverse was calculated before, then it will pull the stored value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinvert()
+    if(!is.null(m)) {
+       message("getting cached data")
+       return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinvert(m)
+    m        
 }
